7f4277c59861a765eafcdd861e5fe24c17ad76a5
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2016-11-15 11:14:45 +0700

    Add project files.

---
+++ LabComproIS-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabComproIS_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse, n1, n2, n3, n4;
+
+            /* Enter Number */
+            Console.Write("Enter a number (1000 - 9999): ");
+            int.TryParse(Console.ReadLine(), out n);
+            /* Receive Number */
+
+            /* First Number (Divided by 1000) Example 2541/1000 = 2.541*/
+            n1 = n / 1000;
+            /* Second Number (Modulo by 1000 and Divided by 100) Example 2541 Mod 1000 ((2541-2000) 541) = / 100 = 5 */
+            n2 = n % 1000 / 100;
+            /* Third Number (Modulo by 100 and Divided by 10) Example 2541 Mod 100 ((2541-2500) 41) = / 10 = 41  */
+            n3 = n % 100 / 10;
+            /* Fourth Number (Modulo by 10) Example 2541 Mod 10 ((2541-2540) 1)*/
+            n4 = n % 10;
+
+            /* Throw up all Number by Unmodulo */
+            reverse = (n4 * 1000) + (n3 * 100) + (n2 * 10) + n1;
+
+            /* Write Number */
+            Console.WriteLine("Reverse number : " + reverse);
+
+            /* Number - Reverse Number (EXAMPLE 2541-1452 = 1089)*/
+            Console.WriteLine(n + " - " + reverse + " = " + (n - reverse));
+
+            Console.ReadKey();
+
+        }
+
+            
+        }
+    }
---
+++ LabComproIS-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LabComproIS-2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabComproIS-2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2009d9f6-6a5a-4e8f-85ce-389fc8b95d82")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
