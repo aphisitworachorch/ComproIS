4d79080ad21b1983ece015ff4410d320e7f4d4b9
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2016-11-22 09:47:12 +0700

    C# : use if else condition in switch case

--- LabComproIS-3.1/Program.cs
+++ LabComproIS-3.1/Program.cs
@@ -8,10 +8,10 @@ namespace LabComproIS_3._1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            int choose1 = 0;
-            int choose2 = 0;
+            int choose1 = 0; /* First Variable for Choosing */
+            int choose2 = 0; /* Second Variable for Quanitity */
             int total1 = 0;
 
             Console.WriteLine("______WELCOME TO COMPRO CAFE______");
@@ -21,24 +21,34 @@ namespace LabComproIS_3._1
             Console.WriteLine("|Choose [4] Ice Tea 35 THB       |");
             Console.WriteLine("|Choose [5] Exit                 |");
             Console.WriteLine("__________________________________");
-            Console.ReadKey();
 
             Console.WriteLine("  ***** Choose Drink Menu *****   ");
             Console.Write("Choose : ");
             int.TryParse(Console.ReadLine(), out choose1);
 
-            switch (choose1)
+            if (choose1 >= 5)
             {
-                case 1 : Console.WriteLine("-- Your Drink is Espresso --"); total1 = choose2 * 40;
+                Console.WriteLine("You not Enter Any menu");
+            }
+            else
+            {
+                Console.WriteLine("  *****  Enter Quantity   *****   ");
+                Console.Write("Choose : ");
+                int.TryParse(Console.ReadLine(), out choose2);
+
+                switch (choose1)
+            {
+                case 1 : Console.WriteLine("---- Your Drink is Espresso ----"); total1 = choose2 * 40;
                     Console.WriteLine("Total {0} X 40.00 Price is {1:N2}", choose2, total1); break;
-                case 2: Console.WriteLine("-- Your Drink is Mocca --"); total1 = choose2 * 40;
+                case 2: Console.WriteLine("---- Your Drink is Mocca ----"); total1 = choose2 * 40;
                     Console.WriteLine("Total {0} X 40.00 Price is {1:N2}", choose2, total1); break;
-                case 3: Console.WriteLine("-- Your Drink is Ice Cocoa --"); total1 = choose2 * 35;
+                case 3: Console.WriteLine("---- Your Drink is Ice Cocoa ----"); total1 = choose2 * 35;
                     Console.WriteLine("Total {0} X 35.00 Price is {1:N2}", choose2, total1); break;
-                case 4: Console.WriteLine("-- Your Drink is Ice Tea --"); total1 = choose2 * 35;
+                case 4: Console.WriteLine("---- Your Drink is Ice Tea ----"); total1 = choose2 * 35;
                     Console.WriteLine("Total {0} X 35.00 Price is {1:N2}", choose2, total1); break;
                 case 5: break;
 
+            }
             }
 
             Console.ReadKey();
